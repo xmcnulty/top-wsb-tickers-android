e48194c271e09f5f7056e4d5fdc7a5dfddc27037
xmcnulty <x.mcnulty@gmail.com>
2022-11-17 20:12:13 -0500

    Added Material3 theme.

--- app/src/main/java/io/xavier/topwsb/presentation/theme/Color.kt
+++ app/src/main/java/io/xavier/topwsb/presentation/theme/Color.kt
@@ -2,7 +2,22 @@ package io.xavier.topwsb.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val PositiveTrend = Color(0xFF65C466)
+val NegativeTrend = Color(0xFFEB4E3D)
+
+// Dark theme
+val DarkBackground = Color(0xFF191919)
+val DarkPrimaryText = Color(0xFFEBEBEB)
+val DarkSecondaryText = Color(0xFFB4B4B4)
+val DarkTopAppBarCollapsed = Color(0xFF212121)
+val DarkDarkSelectedChip = Color(0xFF3B3B3D)
+val DarkSelectedCard = Color(0xFF565656)
+val DarkBackgroundTranslucent = Color(0xFF262626)
--- app/src/main/java/io/xavier/topwsb/presentation/theme/Theme.kt
+++ app/src/main/java/io/xavier/topwsb/presentation/theme/Theme.kt
@@ -1,21 +1,25 @@
 package io.xavier.topwsb.presentation.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import android.app.Activity
+import android.os.Build
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val DarkColorPalette = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val LightColorPalette = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
 
     /* Other default colors to override
     background = Color.White,
@@ -27,18 +31,55 @@ private val LightColorPalette = lightColors(
     */
 )
 
+private val DarkColorScheme = darkColorScheme(
+    background = DarkBackground,
+    onBackground = DarkPrimaryText,
+    surface = DarkBackground,
+    surfaceVariant = DarkDarkSelectedChip,
+    onSurfaceVariant = DarkPrimaryText,
+    primary = DarkBackground,
+    secondary = DarkBackground,
+    tertiary = DarkSelectedCard,
+    onSurface = DarkPrimaryText,
+    onPrimaryContainer = DarkPrimaryText,
+    primaryContainer = DarkSelectedCard,
+    onSecondaryContainer = DarkSecondaryText
+)
+
 @Composable
-fun TopWSBTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun TopWSBTheme(
+    darkTheme: Boolean = true,
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorPalette
+    }
+
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
 
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
     MaterialTheme(
-        colors = colors,
+        colorScheme = colorScheme,
         typography = Typography,
-        shapes = Shapes,
         content = content
     )
 }
